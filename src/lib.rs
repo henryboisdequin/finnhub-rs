fbdc30e2372dc53cb9731de828845ad623f48398
Henry Boisdequin <65845077+henryboisdequin@users.noreply.github.com>
2021-01-24 10:34:02 +0530

    added more tests

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,7 @@ mod utils;
 
 #[cfg(test)]
 mod test {
+    // `cargo t -- --nocapture` to test
     use super::client::*;
     use super::utils::*;
 
@@ -23,6 +24,54 @@ mod test {
         let test_api_key = get_test_api_key();
         let client = Client::new(test_api_key);
         let res = client.symbol_lookup("AAPL".to_string()).await.unwrap();
-        println!("{:?}", res);
+        println!("{:#?}", res);
+    }
+
+    #[tokio::test]
+    async fn stock_symbol_test() {
+        let test_api_key = get_test_api_key();
+        let client = Client::new(test_api_key);
+        let res = client.stock_symbol("US".to_string()).await.unwrap();
+        println!("{:#?}", res);
+    }
+
+    #[tokio::test]
+    async fn company_profile2_test() {
+        let test_api_key = get_test_api_key();
+        let client = Client::new(test_api_key);
+        let res = client.company_profile2("TSLA".to_string()).await.unwrap();
+        println!("{:#?}", res);
+    }
+
+    #[tokio::test]
+    async fn market_news_test() {
+        let test_api_key = get_test_api_key();
+        let client = Client::new(test_api_key);
+        let res = client.market_news("business".to_string()).await.unwrap();
+        println!("{:#?}", res);
+    }
+
+    #[tokio::test]
+    async fn company_news_test() {
+        let test_api_key = get_test_api_key();
+        let client = Client::new(test_api_key);
+        let res = client
+            .company_news(
+                "GOOGL".to_string(),
+                "2020-12-10".to_string(),
+                "2021-01-10".to_string(),
+            )
+            .await
+            .unwrap();
+        println!("{:#?}", res);
+    }
+
+    #[tokio::test]
+    async fn news_sentiment_test() {
+        // TODO: error
+        let test_api_key = get_test_api_key();
+        let client = Client::new(test_api_key);
+        let res = client.news_sentiment("FB".to_string()).await.unwrap();
+        println!("{:#?}", res);
     }
 }
